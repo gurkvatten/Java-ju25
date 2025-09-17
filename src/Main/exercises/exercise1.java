dc78378239e4cba8a31b22d0715cae921808c129
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2025-09-17 11:13:24 +0200

    exercise1

--- src/Main/exercises/exercise1.java
+++ src/Main/exercises/exercise1.java
@@ -1,4 +1,9 @@
 package Main.exercises;
 
 public class exercise1 {
+    static void main(String[] args) {
+        for (int i = 1; i <= 16; i++) {
+            System.out.println(i);
+        }
+    }
 }
