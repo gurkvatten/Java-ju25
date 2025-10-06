60024b73fd538376b1eee3e1b3c04251ebeffbf1
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2025-10-06 14:47:17 +0200

    week 7

---
+++ src/oop4/CollectionDemo.java
@@ -0,0 +1,75 @@
+package oop4;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class CollectionDemo {
+    static void main() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(1);
+        for (var i : list) {
+            System.out.println(i);
+        }
+
+        Set<Integer> IntSet = new HashSet<>();
+        IntSet.add(1);
+        IntSet.add(2);
+        IntSet.add(3);
+        IntSet.add(1);
+        if (IntSet.contains(1)) {
+            System.out.println("yup");
+        }
+
+        for (var i : IntSet) {
+            System.out.println(i);
+        }
+
+        System.out.println("===========");
+
+        Set<MyType> myTypeSet = new HashSet<>();
+
+        myTypeSet.add(new MyType(1));
+        myTypeSet.add(new MyType(2));
+        myTypeSet.add(new MyType(3));
+        myTypeSet.add(new MyType(1));
+
+        for (var i : myTypeSet) {
+            System.out.println(i.getValue());
+        }
+
+    }
+}
+
+class MyType {
+    private int value;
+
+    public MyType(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MyType myType)) return false;
+        return value == myType.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
+
+
+
+
+
+
--- src/oop4/FatInterfaces.java
+++ src/oop4/FatInterfaces.java
@@ -1,4 +1,54 @@
 package oop4;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class FatInterfaces {
+    static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        //List<String> strings = List.of("Test");
+
+            pause(1000);
+
+
+        try {
+            strings.add("Test");
+            strings.get(1);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("cant use add");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds");
+        }
+
+
+        System.out.println(strings.getFirst());
+        example1(new String[] {"1","2"});
+
+        example2("1","2","3");
+    }
+
+    private static void pause(long milliseconds)  {
+
+
+        long t2 = milliseconds;
+        do {
+            long t = System.currentTimeMillis();
+            try {
+                Thread.sleep(t2);
+            } catch (InterruptedException e) {
+
+            } finally {
+                t2 = t2 - System.currentTimeMillis() - t;
+            }
+        } while (t2 > 0);
+    }
+
+    public  static void example1(String[] values) {
+
+    }
+    public  static void example2(String ... values) {
+
+    }
 }
